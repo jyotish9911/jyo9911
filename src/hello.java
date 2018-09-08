a7eda43f38687a955caa291ed1053de8bcffd2dc
jyotish9911 <jyo9911@gmail.com>
2018-09-08 14:32:18 +0530

    chnage

--- src/hello.java
+++ src/hello.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class hello
+ * Servlet implementation class hello hi
  */
 public class hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
